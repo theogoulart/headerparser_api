2822cd938b78eab410433255aaa8dd6825a65a5d
Theo Goulart <theo_goulart@hotmail.com>
2017-03-24 14:58:00 +0000

    add json response format

--- index.js
+++ index.js
@@ -2,8 +2,15 @@ var express = require('express')
 
 var app = express()
 
-app.get('/', function(req, res){
+app.get('', function(req, res){
     
+    var result = {}
+    
+    result.ipaddress    = "ipaddress"
+    result.language     = "language"
+    result.software     = "software"
+    
+    res.send(JSON.stringify(result))
 })
 
 .listen(process.env.PORT || 8080)
